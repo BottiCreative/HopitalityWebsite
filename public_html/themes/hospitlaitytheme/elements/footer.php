226685cb1f6e72778219e6848fc8d72b470b8256
matteld80 <lankyman12>
2014-03-06 12:40:34 +0000

    Live Site Work

---
+++ public_html/themes/hospitlaitytheme/elements/footer.php
@@ -0,0 +1,54 @@
+<?php   defined('C5_EXECUTE') or die("Access Denied."); ?>
+<div class="fullwidthfooter">
+	<div class="row">
+    	<div class="grid-6 columns logofooter">
+        	<img src="<?php  echo $this->getThemePath(); ?>/images/helogo.png" alt=""/>
+            <p>Hospitality Entrepreneur is a collaboration of leading industry professionals and companies, with proven expertise in building successful hospitality businesses. Our goal is to ensure you get better profits, better knowledge and better peace of mind.<br><span>&copy;<?php  echo date('Y')?> Hospitality Entrepreneur ~ All rights reserved</span></p>
+        </div>
+        <div class="grid-4 columns intouch">
+        	<h4>Get in touch</h4>
+            <p>To find out more or to talk to us about getting involved with Hospitality Entrepreneur, please contact us on the details below:</p>
+            <a href=""><img src="<?php  echo $this->getThemePath(); ?>/images/call.png" alt=""/>01865 739 174</a><br>
+            <a href=""><img src="<?php  echo $this->getThemePath(); ?>/images/email.png" alt=""/>info@hospitalityentrepreneur.com</a>
+        </div>
+        <div class="grid-2 columns social">
+        	<h4>Social</h4>
+        		<a href="" target="_blank" class="fbLink" rel="nofollow" ></a>
+              	<a href="" target="_blank" class="twitLink" rel="nofollow" ></a>
+              	<a href="" target="_blank" class="inLink" rel="nofollow" ></a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="VideoModal" class="reveal-modal" data-reveal>
+<div class="row">
+<iframe width="100%" height="100%" src="//www.youtube.com/embed/WGOohBytKTU" frameborder="0" allowfullscreen></iframe>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+</div>
+
+
+
+    <!--<script src="js/foundation.min.js"></script>-->
+    <script src="<?php  echo $this->getThemePath(); ?>/js/foundation/foundation.min.js"></script>
+    <!--<script src="js/foundation/foundation.orbit.js"></script>-->
+    <script src="<?php  echo $this->getThemePath(); ?>/js/foundation/foundation.reveal.js"></script>
+    
+      <script>
+    $(document).foundation(
+	
+	
+	);
+  </script>
+    
+   
+
+
+<?php   Loader::element('footer_required'); ?>
+
+</body>
+</html>
+
+
+
---
+++ public_html/themes/login/elements/members_footer.php
@@ -0,0 +1,13 @@
+<?php   defined('C5_EXECUTE') or die("Access Denied."); ?>
+
+
+footer
+
+
+<?php   Loader::element('footer_required'); ?>
+
+</body>
+</html>
+
+
+
