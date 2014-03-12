65e67f0aba09dd33d2f5626c7d261711f5c18aa8
matteld80 <lankyman12>
2014-03-12 14:15:14 +0000

    Partners Edits

--- public_html/themes/hospitlaitytheme/partners.php
+++ public_html/themes/hospitlaitytheme/partners.php
@@ -5,31 +5,65 @@ $this->inc('elements/header.php'); ?>
 <div class="mainContainer">
 		<div class="row topRowPad">
         
-        	<div class="grid-8 columns">
+		<div class="grid-8 columns mainBody nopadLeft">
+        <main role="main">
+        
 			<?php  
 			$a = new Area('Main');
 			$a->display($c);
 			?>
-			</div>
+        </main>
+            
+		</div>
 
 
 		<div class="grid-4 columns">
-			<?php  
+        
+        <aside>
+         <?php  
+         $a = new GlobalArea('Partners Nav');
+         $a->display();
+         ?>
+         
+         <?php  
 			$a = new Area('Sidebar');
 			$a->display($c);
 			?>
-		</div>	
-        
-          <div class="grid-12 columns partnerFull">
 
-                     	<?php  
+         </aside>
+        
+        
+        
+
+		
+        </div>	
+
+	</div>
+    
+    
+     <div class="partnerFull">
+
+         
+		 <div class="row">
+		 <?php  
+			$a = new Area('Partners Quote');
+			$a->display($c);
+			?>
+       </div>
+
+		</div>  
+        
+        <div class="row">
+		 <?php  
 			$a = new Area('Full Wide');
 			$a->display($c);
 			?>
-
-		</div>     
-        
-	</div>
+       </div>
+    
+    
+    
+    
+    
     </div>
 	
 <?php  $this->inc('elements/footer.php'); ?>
