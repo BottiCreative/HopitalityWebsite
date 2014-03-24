f8027ddb04ca71728465b68ed7b025207a6766c5
Nore <noregabbidon@gmail.com>
2014-03-24 10:20:41 +0000

    Config file updated.

--- public_html/config/site.php
+++ public_html/config/site.php
@@ -8,14 +8,14 @@ define('DB_USERNAME', 'hospitality');
 define('DB_PASSWORD', 'TB0uk1Sl3');
 define('DB_DATABASE', 'hospitalityentrepreneur');
 define('PASSWORD_SALT', 'AvLJ7IlUXRDg5M50ZICDYZ2jtIz1KBVExwI8pncDIr1eotdF2gjGaRPD9xfMHdoa');
+*/
 
-*/ 
 
 /*
 Live Config
 */
-define('DB_SERVER', '79.170.40.225');
-define('DB_USERNAME', 'cl20-hospent01');
-define('DB_PASSWORD', 'Cwqx/6zB6');
-define('DB_DATABASE', 'cl20-hospent01');
+define('DB_SERVER', '10.168.1.50');
+define('DB_USERNAME', 'hospital1_ls5ic');
+define('DB_PASSWORD', 'H05p2014.');
+define('DB_DATABASE', 'hospital1_ls5ic');
 define('PASSWORD_SALT', 'AvLJ7IlUXRDg5M50ZICDYZ2jtIz1KBVExwI8pncDIr1eotdF2gjGaRPD9xfMHdoa');
