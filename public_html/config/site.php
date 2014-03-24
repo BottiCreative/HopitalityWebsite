40bf37ce9d353f75df317855287c39e367e7e6c5
Nore <noregabbidon@gmail.com>
2014-03-24 10:08:37 +0000

    Updated live config site.

--- public_html/config/site.php
+++ public_html/config/site.php
@@ -1,6 +1,21 @@
 <?php 
+
+/* 
+Development Config
+
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'hospitality');
 define('DB_PASSWORD', 'TB0uk1Sl3');
 define('DB_DATABASE', 'hospitalityentrepreneur');
+define('PASSWORD_SALT', 'AvLJ7IlUXRDg5M50ZICDYZ2jtIz1KBVExwI8pncDIr1eotdF2gjGaRPD9xfMHdoa');
+
+*/ 
+
+/*
+Live Config
+*/
+define('DB_SERVER', '79.170.40.225');
+define('DB_USERNAME', 'cl20-hospent01');
+define('DB_PASSWORD', 'Cwqx/6zB6');
+define('DB_DATABASE', 'cl20-hospent01');
 define('PASSWORD_SALT', 'AvLJ7IlUXRDg5M50ZICDYZ2jtIz1KBVExwI8pncDIr1eotdF2gjGaRPD9xfMHdoa');
